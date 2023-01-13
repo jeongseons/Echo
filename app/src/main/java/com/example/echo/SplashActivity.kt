7de7a6a29d3c801f4c9138973e1a5237454562d3
서정선 <slowarirang@gmail.com>
2023-01-13 10:41:44 +0900

    프로필 사진 업로드 추가

--- app/src/main/java/com/example/echo/SplashActivity.kt
+++ app/src/main/java/com/example/echo/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.example.echo.auth.IntroActivity
+import com.example.echo.auth.JoinActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,7 @@ class SplashActivity : AppCompatActivity() {
 
         // postDelayed ({실행할 코드},지연시간)
         Handler().postDelayed({//
-            val intent = Intent(this@SplashActivity, IntroActivity::class.java)
+            val intent = Intent(this@SplashActivity, JoinActivity::class.java)
             startActivity(intent)
         },2000)
 
--- app/src/main/java/com/example/echo/auth/JoinActivity.kt
+++ app/src/main/java/com/example/echo/auth/JoinActivity.kt
@@ -1,36 +1,50 @@
 package com.example.echo.auth
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
-import android.view.View
 import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.echo.MainActivity
 import com.example.echo.R
 import com.example.echo.RetrofitBuilder
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import com.kakao.sdk.user.UserApiClient
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.ByteArrayOutputStream
 
 class JoinActivity : AppCompatActivity() {
 
     var user_gender = ""
+    var user_id = ""
+    lateinit var imgJoinUserProfile: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
 
-        var user_id = ""
-        var etUserNick = findViewById<EditText>(R.id.etUserNick)
-        var etUserBirthdate =  findViewById<EditText>(R.id.etUserBirthdate)
-        var imgUserProfile = findViewById<ImageView>(R.id.imgUserProfile)
+        var etJoinUserNick = findViewById<EditText>(R.id.etJoinUserNick)
+        var etJoinUserBirth =  findViewById<EditText>(R.id.etJoinUserBirth)
+        imgJoinUserProfile = findViewById<ImageView>(R.id.imgJoinUserProfile)
         var rdoUserGender = findViewById<RadioGroup>(R.id.rdoUserGender)
 
         var btnUserJoin = findViewById<Button>(R.id.btnUserJoin)
 
+
+        UserApiClient.instance.me { user, error ->
+            user_id = user?.id.toString()
+        }
+
+        // 성별 선택 이벤트 핸들러
         rdoUserGender.setOnCheckedChangeListener { _, checkedId ->
             Log.d(" ", "RadioButton is Clicked")
             when (checkedId) {
@@ -41,25 +55,34 @@ class JoinActivity : AppCompatActivity() {
                     user_gender = "남"
                 }
             }
+            Log.d("외않되", user_gender)
+
         }
 
-
-
-
-        UserApiClient.instance.me { user, error ->
-            user_id = user?.id.toString()
+        // 프로필 사진 이벤트
+        imgJoinUserProfile.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.INTERNAL_CONTENT_URI
+            )
+            launcher.launch(intent)
         }
 
 
         btnUserJoin.setOnClickListener {
+            var isJoin = true //회원가입 조건 체크
 
-            var user_nick = etUserNick.text.toString()
-            var user_birthdate = etUserBirthdate.text.toString()
-            var user_profile_img = "https://cdn.kormedi.com/wp-content/uploads/2019/09/512.jpg"
+
+            imgUpload(user_id)
+            var user_nick = etJoinUserNick.text.toString()
+            var user_birthdate = etJoinUserBirth.text.toString()
+            var user_profile_img = Firebase.storage.reference.child("$user_id.png").downloadUrl.toString()
+            Log.d("프로필",user_profile_img)
 
             var user = UserVO(user_id, user_nick, user_birthdate, user_profile_img, user_gender)
 
             userJoin(user)
+            imgUpload(user_id)
 
             Toast.makeText(this, "${user_nick}님 환영합니다",
                 Toast.LENGTH_SHORT).show()
@@ -74,33 +97,45 @@ class JoinActivity : AppCompatActivity() {
         call.enqueue(object : Callback<ResponseBody> {
             override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>
             ) {
-                Log.d("외않되", response.body()!!.string())
+                Log.d("test-가입성공", response.body()!!.string())
             }
             override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                Log.d("외않되", t.localizedMessage)
+                Log.d("test-가입실패", t.localizedMessage)
             }
         })
 }
 
-    //성별 라디오버튼 클릭 이벤트 핸들러
-    fun onRadioButtonClicked(view: View) {
-        if (view is RadioButton) {
-            // Is the button now checked?
-            val checked = view.isChecked
+    // 프로필 이미지 업로드
+    fun imgUpload(key:String){
+        val storage = Firebase.storage
+        val storageRef = storage.reference
+        val mountainsRef = storageRef.child("$user_id.png")
 
-            // Check which radio button was clicked
-            when (view.getId()) {
-                R.id.rdoJoinFemale ->
-                    if (checked) {
-                        user_gender = "여"
-                    }
-                R.id.rdoJoinMale ->
-                    if (checked) {
-                        user_gender = "남"
-                    }
-            }
+        // Get the data from an ImageView as bytes
+        imgJoinUserProfile.isDrawingCacheEnabled = true
+        imgJoinUserProfile.buildDrawingCache()
+        val bitmap = (imgJoinUserProfile.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = mountainsRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
         }
-        Log.d("외않되", user_gender)
-
     }
+
+    // 선택한 프로필 사진 보여주기
+    val launcher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            imgJoinUserProfile.setImageURI(it.data?.data)
+        }
+    }
+
+
 }
