c4f8b0885b457862603286f10787b4ef1f2b5c7b
ipjyie <iusp0054@gamil.com>
2023-01-18 11:42:44 +0900

    지도 API Key 수정

--- app/src/main/java/com/example/echo/path/MapFragment.kt
+++ app/src/main/java/com/example/echo/path/MapFragment.kt
@@ -1,52 +1,75 @@
 package com.example.echo.path
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context.LOCATION_SERVICE
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import android.location.LocationRequest
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.app.ActivityCompat
+import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
 import com.example.echo.R
-import com.google.android.gms.location.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.PolylineOptions
 
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
     private val googleMap: GoogleMap? = null
-    private var mLocationManager: LocationManager? = null
-    private var mLocationListener: LocationListener? = null
     private lateinit var mView: MapView
     private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
     lateinit var mLastLocation: Location
-    private lateinit var mLocationRequest: LocationRequest
     private var REQUEST_PERMISSION_LOCATION = 10
     private var mLatitude: Double = 0.0
     private var mLongitude: Double = 0.0
-
-
     lateinit var manager : LocationManager
     lateinit var locationListener : LocationListener
     lateinit var myLocation : LatLng
+    lateinit var user_id : String
+    lateinit var coursedt : String
+    var open_yn : Boolean = false
+    var course_type : String = "A"
+    private var isTracking = false
+    private var pathPoints = mutableListOf<LatLng>()
+    private val currentMarker: Marker? = null
+    private val GPS_ENABLE_REQUEST_CODE = 2001
+    private val UPDATE_INTERVAL_MS = 1000 // 1초
+    private val FASTEST_UPDATE_INTERVAL_MS = 500 // 0.5초
+    private val PERMISSIONS_REQUEST_CODE = 100
+    var needRequest = false
+    var REQUIRED_PERMISSIONS = arrayOf<String>(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    var mCurrentLocatiion: Location? = null
+    var currentPosition: LatLng? = null
+    private val mFusedLocationClient: FusedLocationProviderClient? = null
+    private val locationRequest: LocationRequest? = null
+    private val location: Location? = null
+    private val mLayout: View? = null
 
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -54,80 +77,77 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_map, container, false)
+        val btnMapRecordStart = view.findViewById<Button>(R.id.btnMapRecordStart)
+        val btnMapRecordEnd = view.findViewById<Button>(R.id.btnMapRecordEnd)
+        val tvMapCurrentTime = view.findViewById<TextView>(R.id.tvMapCurrentTime)
+        val tvMApCurrentLocation = view.findViewById<TextView>(R.id.tvMApCurrentLocation)
 
-        val imgMapRecordStart = view.findViewById<Button>(R.id.imgMapRecordStart)
-//        val imgMapCurrentLocation = view.findViewById<ImageView>(R.id.imgMapCurrentLocation)
 
+        //경로저장하는 Array polilines
+        var polilines = emptyArray<LatLng>()
+
+        btnMapRecordEnd.visibility = View.INVISIBLE
+
+        btnMapRecordStart.setOnClickListener {
+            Log.d("산행중gps값받아오기","${mLatitude},${mLongitude}")
+            val polyLineOptions = PolylineOptions().width(5f).color(Color.BLUE)
+            /*polyLineOptions.add(myLocation)*/
+
+            btnMapRecordStart.visibility = View.INVISIBLE
+            btnMapRecordEnd.visibility = View.VISIBLE
+
+        }
+
+        btnMapRecordEnd.setOnClickListener {
+
+            val intent = Intent(requireContext(), MapSaveActivity::class.java)
+            startActivity(intent)
+        }
 
         mView = view.findViewById(R.id.map) as MapView
         mView.onCreate(savedInstanceState)
         mView.getMapAsync(this)
 
-        mFusedLocationProviderClient =
-            LocationServices.getFusedLocationProviderClient(requireContext())
-
-
-// Instantiates a new Polyline object and adds points to define a rectangle
-        val polylineOptions = PolylineOptions().width(5f).color(Color.BLUE)
-            .add(LatLng(35.11049333333334, 126.87776166666666))
-            .add(LatLng(34.14671833333333, 125.92025333333335)) // North of the previous point, but at the same longitude
-
-        googleMap?.addPolyline(polylineOptions)
-
-
-        imgMapRecordStart.setOnClickListener {
-
-            val polyLineOptions = PolylineOptions().width(5f).color(Color.BLUE)
-            var testCourese = LatLng(35.11049333333334,126.87776166666666)
-            var testCourese2 = LatLng(35.14671833333333,126.92025333333335)
-            /*polyLineOptions.add(myLocation)*/
-            polyLineOptions.add(testCourese)
-            polyLineOptions.add(testCourese2)
-            googleMap?.addPolyline(polyLineOptions)
-//            Log.d("산행중 gps값 받아오기","${mLatitude},${mLongitude}")
-        }
-
-//        imgMapCurrentLocation.setOnClickListener {
-//            googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 19F))
-//            googleMap?.moveCamera(CameraUpdateFactory.newLatLng(myLocation))
-//        }
-
                 return view
-
     }
+
+
 
 
     //Map실행
     override fun onMapReady(googleMap: GoogleMap) {
-        //퍼미션 허가
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) !=
-            PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                requireContext() as Activity,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                0
-            )
-        } else {
-            //현재위치 받아오기
-            manager = requireContext().getSystemService(LOCATION_SERVICE) as LocationManager
-            locationListener = object : LocationListener{
-                override fun onLocationChanged(location: Location) {
 
+
+        //퍼미션 허가
+        if (ActivityCompat.checkSelfPermission( requireContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+           ActivityCompat.requestPermissions( requireContext() as Activity, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION), 0 )
+            return
+        } else {
+
+            //현재위치 받아오기
+            var manager = requireContext().getSystemService(LOCATION_SERVICE) as LocationManager
+            locationListener = object : LocationListener {
+
+                val location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+
+                override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                    super.onStatusChanged(provider, status, extras)
+                    // provider의 상태가 변경될때마다 호출
+                    // deprecated
+                }
+
+                override fun onLocationChanged(location: Location) {
 
 
                     mLatitude = location.latitude
                     mLongitude = location.longitude
                     myLocation = LatLng(mLatitude, mLongitude)
-                    googleMap.addMarker(MarkerOptions().position(myLocation).title("현재위치"))
-                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 19F))
+                    Log.d("현재gps받아오기", "${location?.latitude},${location?.longitude}")
+
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 18F))
                     googleMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation))
-                    Log.d("현재 gps 받아오기","${location?.latitude},${location?.longitude}")
-
+                    pathPoints.add(myLocation)
                 }
-
 
                 override fun onProviderEnabled(provider: String) {
                     super.onProviderEnabled(provider)
@@ -138,12 +158,11 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                 }
             }
             //업데이트
-            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                10_000L,10f,locationListener)
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000
+                , 10.0f, locationListener)
+
         }
     }
-
-
 
 
     override fun onStart() {
@@ -159,6 +178,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     override fun onResume() {
         super.onResume()
         mView.onResume()
+
     }
 
     override fun onPause() {
---
+++ app/src/main/java/com/example/echo/path/MapVO.kt
@@ -0,0 +1,13 @@
+package com.example.echo.path
+
+class MapVO(var course_seq: Int?,
+            var user_id: String = "",
+            var coursedt: String = "",
+            var lat: Double,
+            var lng: Double,
+            var alt: Double,
+            var open_yn: Boolean,
+            var course_type: String = ""
+             ){
+
+}
