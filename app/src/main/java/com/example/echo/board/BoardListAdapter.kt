ee5fd50b1e4df172c047b5239f65bd7accb59482
서정선 <slowarirang@gmail.com>
2023-02-01 12:37:41 +0900

    댓글수 카운트, 가입신청 다이얼로그 수정

--- app/src/main/java/com/example/echo/board/BoardListAdapter.kt
+++ app/src/main/java/com/example/echo/board/BoardListAdapter.kt
@@ -43,6 +43,7 @@ class BoardListAdapter(var context: Context, var BoardList:ArrayList<BoardListVO
         val tvBoardWriter: TextView
         val tvBoardRecoCount: TextView
         val tvBoardMntName : TextView
+        val tvBoardCommCount : TextView
         val imgBoardPic : ImageView
 
         init {
@@ -51,6 +52,7 @@ class BoardListAdapter(var context: Context, var BoardList:ArrayList<BoardListVO
             tvBoardDate = itemView.findViewById(R.id.tvBoardDate)
             tvBoardRecoCount = itemView.findViewById(R.id.tvBoardRecoCount)
             tvBoardMntName = itemView.findViewById(R.id.tvBoardMntName)
+            tvBoardCommCount = itemView.findViewById(R.id.tvBoardCommCount)
             imgBoardPic = itemView.findViewById(R.id.imgBoardPic)
 
             // 각 게시글 클릭 이벤트 - 게시글 내부로 이동
@@ -97,6 +99,7 @@ class BoardListAdapter(var context: Context, var BoardList:ArrayList<BoardListVO
                 holder.tvBoardDate.text = filteredList.board_dt.substring(0,filteredList.board_dt.length-3)
                 holder.tvBoardRecoCount.text = filteredList.board_reco_cnt.toString()
                 holder.tvBoardMntName.text = filteredList.mnt_name
+                holder.tvBoardCommCount.text = filteredList.board_cmt_cnt.toString()
                 if(filteredList.board_file.isEmpty()) {
                     holder.imgBoardPic.visibility = View.INVISIBLE
                 } else{
