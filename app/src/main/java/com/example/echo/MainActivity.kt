9b2f335ff6b3c09da695bc1ad9bf35b18fc11408
m1otic <parkminook16@naver.com>
2023-02-03 15:35:50 +0900

    sos 완

--- app/src/main/java/com/example/echo/MainActivity.kt
+++ app/src/main/java/com/example/echo/MainActivity.kt
@@ -1,50 +1,42 @@
 package com.example.echo
 
-import android.content.ContentValues.TAG
+//import com.example.echo.path.MapFragment2
+
+import android.app.ActivityManager
+import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.example.echo.auth.IntroActivity
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ProcessLifecycleOwner
 import com.example.echo.board.BoardFragment
-import com.example.echo.board.BoardPostFragment
 import com.example.echo.group.GroupFragment
 import com.example.echo.myPage.MyPageFragment
-import com.example.echo.path.MapFragment
 import com.example.echo.path.MapFragment2
-//import com.example.echo.path.MapFragment2
-import com.example.echo.path.PathFragment
 import com.example.echo.service.SensorService
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
 import com.kakao.sdk.common.util.Utility
-import com.kakao.sdk.user.UserApiClient
-import com.kakao.sdk.user.model.User
-import de.hdodenhof.circleimageview.CircleImageView
-import kotlinx.android.synthetic.main.activity_map_save.*
-import okhttp3.ResponseBody
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.http.Tag
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LifecycleObserver {
     var user_id = ""
     var moveCk = ""
+
+    var check = false
+
+    private var mSensorManager: SensorManager? = null
+    private var mAccelerometer: Sensor? = null
+    private var mShakeDetector: SensorService? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Intent(this, SensorService::class.java).also { intent ->
-            startService(intent)
-        }
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
 
         val flMain = findViewById<FrameLayout>(R.id.flMain)
         val bnvMain = findViewById<BottomNavigationView>(R.id.bnvMain)
@@ -52,6 +44,36 @@ class MainActivity : AppCompatActivity() {
 
         //하단 메뉴바 아이콘 색상 틴트 빼기
         bnvMain.setItemIconTintList(null);
+        val intent = Intent(applicationContext, SensorService::class.java)
+        startService(intent)
+
+        // ShakeDetector initialization
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mAccelerometer = mSensorManager!!
+            .getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        mShakeDetector = SensorService()
+        mShakeDetector!!.setOnShakeListener(object : SensorService.OnShakeListener {
+            override fun onShake(count: Int) {
+                //감지시 할 작업 작성
+                val intent = Intent(applicationContext, CountActivity::class.java)
+                startActivity(intent)
+
+                //백그라운드일시 포그라운드로 전환
+                val am = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+                val tasks = am.getRunningTasks(100)
+                if (!tasks.isEmpty()) {
+                    val taskSize = tasks.size
+                    for (i in 0 until taskSize) {
+                        val taskInfo = tasks[i]
+                        if (taskInfo.topActivity!!.packageName == packageName) {
+                            check = true
+                            am.moveTaskToFront(taskInfo.id, 0)
+                        }
+                    }
+                }
+
+            }
+        })
 
 
         var keyHash = Utility.getKeyHash(this)
@@ -119,6 +141,20 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+
+
+    override fun onResume() {
+        super.onResume()
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager!!.registerListener(
+            mShakeDetector,
+            mAccelerometer,
+            SensorManager.SENSOR_DELAY_UI
+        )
+    }
+
+
 
 
 //    fun kakaoLogout() {
--- app/src/main/java/com/example/echo/service/SensorService.kt
+++ app/src/main/java/com/example/echo/service/SensorService.kt
@@ -1,130 +1,97 @@
 package com.example.echo.service
 
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.os.*
-import android.os.Process.THREAD_PRIORITY_BACKGROUND
+import android.os.IBinder
 import android.util.Log
-import android.widget.Toast
-import com.example.echo.CountActivity
-import java.lang.Math.sqrt
-import java.lang.Process
+import com.example.echo.MainActivity
 
 
 class SensorService : Service(), SensorEventListener {
 
-    private var serviceLooper: Looper? = null
-    private var serviceHandler: ServiceHandler? = null
+    // listener
+    private var mListener: OnShakeListener? = null
 
-    val TAG: String = "로그"
+    // 시간 기록용
+    private var mShakeTimestamp: Long = 0
 
-    private var accel: Float = 0.0f //초기
-    private var accelCurrent: Float = 0.0f //이동하는 치수
-    private var accelLast: Float = 0.0f
+    // 횟수
+    private var mShakeCount = 0
 
-    //당장이 아니라 나중에 값을 넣겠다. 온크레이트 이후에 설정을 넣겠다.
-    private lateinit var sensorManager: SensorManager
+    // listener setting
+    fun setOnShakeListener(listener: OnShakeListener?) {
+        mListener = listener
+    }
 
-    // 메인 스레드로부터 메세지를 전달받을 핸들러 선언
-    private inner class ServiceHandler(looper: Looper) : Handler(looper) {
+    // listener interface
+    interface OnShakeListener {
+        fun onShake(count: Int)
+    }
 
-        override fun handleMessage(msg: Message) {
-            // 보통 서비스에서는 파일 다운로드 같은 작업을 수행함
-            // 이 예제에서는 sleep. 5초를 주도록 함
-            try {
-                Thread.sleep(5000)
-            } catch (e: InterruptedException) {
-                // Restore interrupt status.
-                Thread.currentThread().interrupt()
+    // 정확도가 변할 때? 사용하지 않는다
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // ignore
+    }
+
+    // 센서 method.
+    override fun onSensorChanged(event: SensorEvent) {
+        if (mListener != null) {
+            // x,y,z 축의 값을 받아온다
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            // 중력 가속도값으로 나눈 값으로 만든다
+            val gX = x / SensorManager.GRAVITY_EARTH
+            val gY = y / SensorManager.GRAVITY_EARTH
+            val gZ = z / SensorManager.GRAVITY_EARTH
+            // gforce는 중력가속도를 포함하는 물체가 받는 힘
+            // 1일때는 평소에 받는 중력(정지)
+            // 1이하일때(아래로 떨어지며 힘을 덜받을 때)
+            // 1이상일 때(위로 올라가면서 힘을 더 받을 때)
+            // 단순히 힘의 크기를 계산하기 때문에 피타고라스로 구한다
+            // gForce will be close to 1 when there is no movement.
+            val gForce = Math.sqrt((gX * gX + gY * gY + gZ * gZ).toDouble()).toFloat()
+            // 진동을 감지했을 때
+            // gforce가 기준치 이상일 경우
+            if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+                val now = System.currentTimeMillis()
+                // 진동 간격이 너무 짧을 때는 무시
+                // ignore shake events too close to each other (500ms)
+                if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
+                    return
+                }
+                // 3초 이상 걸렸을 때 reset한다
+                // reset the shake count after 3 seconds of no shakes
+                if (mShakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
+                    mShakeCount = 0
+                }
+                // 업데이트한다
+                mShakeTimestamp = now
+                mShakeCount++
+
+                // 흔들렸을 때 행동을 설정한다
+                mListener!!.onShake(mShakeCount)
             }
-            // 서비스를 사용하였다면 서비스를 종료해 주어야 함.
-            // 아래 메소드는 작업 startId가 가장 최신일때만 서비스를 stop하게 함
-            // 이렇게 하면 동시에 여러 작업할 때, 모든작업이 끝나야 stop이 된다
-            // 이게뭔지는 이후 설명에서 나옴
-            stopSelf(msg.arg1)
         }
     }
+    companion object {
+        // 센서 이벤트를 들을 listener를 충족하는 class
+        // 중력, 중력가속도을 기준으로 삼아서 진동, 움직임을 측정한다.
+        // 흔들림 감지할 때 기준이 되는 가해지는 힘
+        private const val SHAKE_THRESHOLD_GRAVITY = 2.7f
 
-    override fun onCreate() {
+        // 흔들림 감지할때 최소 0.5초를 기준으로 측정한다.
+        private const val SHAKE_SLOP_TIME_MS = 500
 
-        Log.d(TAG, "MainActivity - onCreate() called")
-        this.sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-
-        accel = 10f
-        accelCurrent = SensorManager.GRAVITY_EARTH //지구 중력값 주기
-        accelLast = SensorManager.GRAVITY_EARTH
-
-        // 서비스는 메인스레드에서 동작하므로 서비스의 작업은 따로 스레드를 선언해 주어야 한다.
-        // CPU priority를 background로 선언하여 ui의 버벅임을 방지해야 한다.
-        HandlerThread("ServiceStartArguments").apply {
-            start()
-
-            // 스레드의 루퍼를 얻어와 핸들러를 만들어준다.
-            serviceLooper = looper
-            serviceHandler = ServiceHandler(looper)
-        }
+        // 흔드는 횟수는 3초마다 초기화
+        private const val SHAKE_COUNT_RESET_TIME_MS = 3000
     }
 
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show()
-
-        Log.d(TAG, "MainActivity - onResume() called")
-        sensorManager.registerListener(this, sensorManager
-            .getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-            ,SensorManager.SENSOR_DELAY_NORMAL)
-
-        // For each start request, send a message to start a job and deliver the
-        // start ID so we know which request we're stopping when we finish the job
-        serviceHandler?.obtainMessage()?.also { msg ->
-            msg.arg1 = startId
-            serviceHandler?.sendMessage(msg)
-        }
-
-        // If we get killed, after returning from here, restart
-        return START_STICKY
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
     }
-
-    //흔들었을때 센서 감지
-    override fun onSensorChanged(event: SensorEvent?) {
-        Log.d(TAG, "MainActivity - onSensorChanged() called")
-
-        val x:Float = event?.values?.get(0) as Float
-        val y:Float = event?.values?.get(0) as Float
-        val z:Float = event?.values?.get(0) as Float
-
-        accelLast = accelCurrent
-        accelCurrent = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
-
-        val delta: Float = accelCurrent - accelLast
-
-        accel = accel * 0.9f + delta
-
-        //액셀 치수가 30이 넘어가면 흔들었다고 휴대폰이 판단한다.
-        if(accel > 30){
-            Log.d(TAG, "흔들었다.")
-            val test = Intent(applicationContext, CountActivity::class.java)
-            startActivity(test)
-        }
-    }
-
-    //센서에 정확도 변경되면 호출된다.
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        Log.d(TAG, "MainActivity - onAccuracyChanged() called")
-    }
-
-    override fun onBind(intent: Intent): IBinder? {
-        // We don't provide binding, so return null
-        return null
-    }
-
-    override fun onDestroy() {
-        sensorManager.unregisterListener(this)
-        Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show()
-    }
-
 }
